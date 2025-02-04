b4ae6d0ce7eab81375fb6c50efc2d04f4001461f
Taha Munawar <109829503+tahamunawar@users.noreply.github.com>
2025-02-04 18:08:48 +0500

    Add drawing functionality
    Can now draw in the grid. Must press the mouse button and hover
    simulatenously. All other required features, of clearing grid, making
    new grid implemented. Only styling left

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # odin-etchasketch
+
+Slight modification in the functionality specified by TOP. Instead of the box changing color when the mouse is simply hovered over it, the box will change will color when the mouse button is being pressed. In my opinion this makes for a more "usable" project.
--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 let dimension = 16;
 const dabba = document.getElementById('grid-box');
+const clearbutton = document.querySelector('#butt1');
 const newbutton = document.getElementById('butt2');
-
+const linecontainer = document.querySelector(".dimension-size");
+const line = document.createElement("p");
+line.textContent = `Current Number of Boxes per Dimension: ${dimension}`;
+linecontainer.append(line);
 function newGrid(){
     dabba.textContent = "";
     let newDimension = prompt("Enter the new number of squares per grid. Ensure number ranges from 1-100");
@@ -16,15 +20,42 @@ function newGrid(){
 }
 
 function createGrid(dimension){
+    line.textContent = `Current Number of Boxes per Dimension: ${dimension}`;
     for (let i=0;i<dimension*dimension;i++) {
         const drawingBox = document.createElement("div");
         drawingBox.style.width = `${100/dimension}%`;
-        drawingBox.style.border = "0.5px solid black";
+        drawingBox.style.border = "0.1px solid black";
         drawingBox.classList.add("drawing-box");
         drawingBox.style.boxSizing = "border-box";
         dabba.append(drawingBox);
     }
+    drawGrid();
+}
+
+function clearGrid(){
+    const boxList = document.querySelectorAll(".drawing-box");
+    boxList.forEach((item) => {
+        item.style.backgroundColor = "white";
+    })
+}
+
+function drawGrid(){
+    const boxList = document.querySelectorAll(".drawing-box");
+    boxList.forEach((item) => {
+        item.addEventListener("mouseenter", function(e) { // got this from stack overflow goated
+            if(e.buttons == 1){
+                item.style.backgroundColor = "black";
+                console.log("box colored while mouse being hovered");
+            }
+        })
+        item.addEventListener("mousedown", () => {
+            item.style.backgroundColor = "black";
+            console.log("box colored on just a single click");
+        } )
+    })
 }
 
 newbutton.addEventListener('click',newGrid);
+clearbutton.addEventListener('click',clearGrid);
 createGrid(dimension);
+drawGrid();
